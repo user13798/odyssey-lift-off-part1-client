038e90a051d13ca145cfe330d62fffcef65e78b1
user13798 <71964046+user13798@users.noreply.github.com>
2022-07-07 20:19:13 +0300

    1

--- src/pages/tracks.js
+++ src/pages/tracks.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout } from "../components";
+import { gql } from "@apollo/client";
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
+
 const Tracks = () => {
   return <Layout grid> </Layout>;
 };
